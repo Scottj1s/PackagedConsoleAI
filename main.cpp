b3087f1074cad192f95f727ef52476bd7d9fcabf
Scott Jones <scottj1s@gmail.com>
2025-05-06 13:58:56 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+
+#include <winrt/Microsoft.Windows.Management.Deployment.h>
+#include <winrt/Microsoft.Windows.AI.h>
+#include <winrt/Microsoft.Windows.AI.Search.Experimental.ApplicationContentIndex.h>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+int main()
+{
+    init_apartment();
+    auto x = winrt::Microsoft::Windows::AI::Search::Experimental::ApplicationContentIndex::ApplicationContentIndexer::GetReadyState();
+    x;
+}
